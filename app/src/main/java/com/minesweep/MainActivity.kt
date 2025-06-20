2886ec725892892b544b8f718ef5d756e3822dd7
JoaquimGon <joaquimmcgoncalves@gmail.com>
2025-06-20 01:33:24 +0100

    Criacao Proejto
    Criacao projeto e variável mutável para matriz de 2 dimensões

---
+++ app/src/main/java/com/minesweep/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.minesweep
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.minesweep.ui.theme.MinesweepTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MinesweepTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Grelha() {
+    val grid = remember {
+        mutableStateListOf<SnapshotStateList<Int>>().apply {
+            repeat(6) {
+                add(MutableList(4) { 0 }.toMutableStateList())
+            }
+        }
+    }
+}
+
+
+
